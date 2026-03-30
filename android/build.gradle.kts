53c107597cfe2fefe58b5cf5221eeb0fa97032fc
wangxinyueg-code <wangxinyueg@gmail.com>
2026-03-30 18:12:21 +0800

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,8 @@
+plugins {
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+}
+
 allprojects {
     repositories {
         maven(url = "https://maven.aliyun.com/repository/google")
